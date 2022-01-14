72fec45dc33a1a3b59c23da4e22ebc57acbf2196
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2022-01-15 01:26:49 +0530

    ➕ node version & update license in package.json 🛠 refactor .env token name

--- test/token.test.ts
+++ test/token.test.ts
@@ -4,7 +4,7 @@ import { BigNumber, Contract, ContractFactory } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 // Token metadata
-const tokenName = "Token";
+const tokenName = "CryptonToken";
 const symbol = "CRPT";
 const decimals = 18;
 const feeRate = 150; // 1.5%
@@ -26,7 +26,7 @@ describe("Token", function () {
 
   before(async () => {
     [owner, alice, bob] = await ethers.getSigners();
-    Token = await ethers.getContractFactory("Token");
+    Token = await ethers.getContractFactory(tokenName);
   });
 
   beforeEach(async () => {
@@ -324,7 +324,7 @@ describe("Token", function () {
     });
   });
 
-  // In out tests Alice has the MINTER_ROLE
+  // In our tests Alice has the MINTER_ROLE
   describe("Minting", function () {
     it("Should not be able to mint tokens without MINTER_ROLE", async () => {
       const mintAmount = tenTokens;
